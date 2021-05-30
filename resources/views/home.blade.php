43902d8d20eaf7b9069ef96386cf8c2393ebe065
Clarissa <ic19b054@technikum-wien.at>
2021-05-30 10:19:26 +0200

    added welcome text to home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,11 @@
             @if (session('status'))
                 <query-message :success="true" :fail="false" message="{{ session('status') }}"></query-message>
             @endif
-            <p>Welcome <strong>{{ auth()->user()->name }}</strong></p>
+            <p>Welcome <strong>{{ auth()->user()->name }}</strong>.</p>
+                <p>Hey, you. You’re finally awake. You were trying to cross the border,
+                    right? Walked right into that Imperial ambush, same as us, and that
+                    thief over there.</p>
+                <p><img src="{{asset('img/logo_transparent.png')}}" width="30" height="14" ><strong> Welcome to Magic Almanac.</strong></p>
         </example-component>
     </section>
 @endsection
